2ec0e8a3369d8e728f282373e603de52e57d45f9
martinvegabollmann <martinvegabollmann>
2023-07-30 00:00:11 +0100

    Another Updated

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-const buttons = document.querySelector('#buttons');
-const username = document.querySelector('#username');
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelector('#buttons');
+  const username = document.querySelector('#username');
 
-// Usa directamente 'firebase.auth().onAuthStateChanged'
-firebase.auth.onAuthStateChanged((user) => {
-  if (user) {
-    console.log(user)
-    buttons.innerHTML = /*html*/ `
-    <button class="btn btn-outline-danger mr-2">Logout</button>
-    `;
-  } else {
-    console.log('Does not exist user');
-    buttons.innerHTML = /*html*/ `
-      <button class="btn btn-outline-success mr-2" id='access'>Access</button>
-    `;
-  }
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log(user);
+      buttons.innerHTML = /*html*/ `
+        <button class="btn btn-outline-danger mr-2" id="btnLogout">Logout</button>
+      `;
+    } else {
+      console.log('No existe un usuario');
+      buttons.innerHTML = /*html*/ `
+        <button class="btn btn-outline-success mr-2" id="btnAccess">Access</button>
+      `;
+      startSesion();
+    }
+  });
+
+  const startSesion = () => {
+    const btnAccess = document.querySelector('#btnAccess');
+    btnAccess.addEventListener('click', async () => {
+      console.log('hiciste clic');
+      try {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        await firebase.auth().signInWithPopup(provider)
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
 });
